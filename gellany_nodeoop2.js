e2ce4bb63503ee239f4e4b97908db0815b93ff87
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-26 17:19:28 +0000

    Add files via upload

--- gellany_nodeoop2.js
+++ gellany_nodeoop2.js
@@ -1,17 +1,34 @@
 const express = require('express')
 var parseUrl = require('body-parser')
 const app = express()
-
-
 let encodeUrl = parseUrl.urlencoded({ extended: false })
 
+class gellany_nodeoop2 {
+  constructor(form = '', port = 0) {
+  this.port = port
+  this.form = form
+}
+
+
+main() {
+//console.log(this.form)
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/form.html')
+  res.sendFile(__dirname+form);
+  
 });
 
 app.post('/', encodeUrl, function(req, res) {
   console.log('Form request:', req.body)
   res.sendStatus(200)
 });
+//console.log(this.form)
+app.listen(this.port)
 
-app.listen(4000)
+//console.log(this.port)
+//console.log(this.form)
+}
+
+}
+
+const run = new gellany_nodeoop2(form = '/form.html', port = 4000).main()
+run
